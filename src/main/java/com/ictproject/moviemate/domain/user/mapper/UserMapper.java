48b2f234c8a6375416db2facaafa02c19b124164
yb <yb725v@naver.com>
2024-04-15 11:17:30 +0900

    페이지 로직, 매퍼 추가

--- src/main/java/com/ictproject/moviemate/domain/user/mapper/UserMapper.java
+++ src/main/java/com/ictproject/moviemate/domain/user/mapper/UserMapper.java
@@ -1,4 +1,20 @@
 package com.ictproject.moviemate.domain.user.mapper;
 
+import com.ictproject.moviemate.domain.user.User;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
 public interface UserMapper {
+
+	// 회원 가입
+	void insertUser(User user);
+
+	// 회원 정보 수정
+	void modifyUser(User user);
+
+	// 회원 정보 단일 조회
+	User findUser(int userId);
+
+	// 회원 탈퇴
+	void deleteUser(int userId);
 }
