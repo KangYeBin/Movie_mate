d660ddb08616c0ff351e8affd18d17a9dac7da31
yb <yb725v@naver.com>
2024-04-19 12:49:30 +0900

    마이 페이지 찜한 영화 불러오기

--- src/main/java/com/ictproject/moviemate/domain/user/controller/UserController.java
+++ src/main/java/com/ictproject/moviemate/domain/user/controller/UserController.java
@@ -1,12 +1,33 @@
 package com.ictproject.moviemate.domain.user.controller;
 
+import com.ictproject.moviemate.domain.movie.dto.MovieResponseDTO;
+import com.ictproject.moviemate.domain.user.User;
+import com.ictproject.moviemate.domain.wish.service.WishService;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
+@Slf4j
+@RequiredArgsConstructor
 public class UserController {
+
+    private final WishService wishService;
+
     @GetMapping("/my")
-    public String myPage(){
+    public String myPage(Model model, HttpSession session){
+
+        User loginUser = (User) session.getAttribute("login");
+        List<MovieResponseDTO> dto = wishService.findWish(loginUser.getUserId());
+
+        model.addAttribute("movie", dto);
+        log.info("movie : {}", dto);
+
 
         return "mypage";
     }
--- src/main/java/com/ictproject/moviemate/domain/wish/mapper/WishMapper.java
+++ src/main/java/com/ictproject/moviemate/domain/wish/mapper/WishMapper.java
@@ -1,7 +1,10 @@
 package com.ictproject.moviemate.domain.wish.mapper;
 
+import com.ictproject.moviemate.domain.movie.Movie;
 import com.ictproject.moviemate.domain.wish.Wish;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface WishMapper {
@@ -11,5 +14,8 @@ public interface WishMapper {
 
 	// 찜 해제
 	void deleteWish(Wish wish);
+
+	// 찜한 영화 목록 가져오기
+	List<Movie> findWish(int userId);
 
 }
