0452d82566d4c6dc07675a11766e3988aa2335f3
seungjae0908 <tmtmdd123@naver.com>
2024-04-23 16:46:41 +0900

    모달페이지 css수정, select 수정

--- src/main/resources/static/assets/js/movieinfo.js
+++ src/main/resources/static/assets/js/movieinfo.js
@@ -1,5 +1,3 @@
-
-
         /* 리뷰별개수 */
         const drawStar = (target) => {
             document.querySelector(`.star span`).style.width = `${target.value * 10}%`;
@@ -29,9 +27,9 @@
 
             const htmlDummy = new Array(_size).fill().map((_, i) => {
                 return `<circle class='star'
-        cx=${getRandomX()}
-        cy=${getRandomY()}
-        r=${randomRadius()}
+        cx=\${getRandomX()}
+        cy=\${getRandomY()}
+        r=\${randomRadius()}
         className="star" />`
             }).join('');
 
