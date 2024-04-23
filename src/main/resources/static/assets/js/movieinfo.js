bbd09e7e17d9b72e3150e8b1785fb542245bdb91
seungjae0908 <tmtmdd123@naver.com>
2024-04-23 12:31:06 +0900

    모달페이지 수정

--- src/main/resources/static/assets/js/movieinfo.js
+++ src/main/resources/static/assets/js/movieinfo.js
@@ -1,3 +1,5 @@
+
+
         /* 리뷰별개수 */
         const drawStar = (target) => {
             document.querySelector(`.star span`).style.width = `${target.value * 10}%`;
@@ -39,14 +41,29 @@
         window.onload = () => {
             makeStars();
         }
-    
+        var reviewSwiper =new Swiper('.review-swiper-custom', {
+            speed: 800, // 슬라이드 속도
+            slidesPerView: 4, // 한 번에 보여질 슬라이드 수
+            slidesPerGroup: 4,
+            spaceBetween: 10, // 이미지 간격
+            loop: false, // 슬라이드 루프 설정 비활성화
+            pagination: {
+                el: ".pagination_bullet",
+                clickable: true,
+                type: 'bullets',
+                renderBullet: function (index, className) {
+                    return '<span class="' + className + '">' + (index + 1) + "</span>";
+                },
+            },
+        })
 
-        // 찜 만들기
+        // 찜 하트
         var like = document.getElementById("like")
 
         like.addEventListener('click', function () {
             like.classList.toggle('active')
         });
+
 
         // 공감 만들기
         var thumb = document.getElementById("thumb")
@@ -56,28 +73,3 @@
         });
         
         
-// 모달 열기 버튼
-var openModalBtn = document.getElementById("openModal");
-
-// 모달
-var modal = document.getElementById("reviewModal");
-
-// 모달 닫기 버튼
-var closeModalBtn = document.getElementsByClassName("close")[0];
-
-// 모달 열기 버튼에 클릭 이벤트 리스너 추가
-openModalBtn.onclick = function() {
-    modal.style.display = "block";
-}
-
-// 모달 닫기 버튼에 클릭 이벤트 리스너 추가
-closeModalBtn.onclick = function() {
-    modal.style.display = "none";
-}
-
-// 모달 바깥을 클릭하면 모달이 닫히도록 함
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
